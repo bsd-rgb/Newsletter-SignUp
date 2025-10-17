42ce4ffa2e0ba51c543a5270cf297c4219ebfaa7
Brandi <brandi.davis2483@gmail.com>
2025-10-16 21:26:11 -0500

    [x] Updated script.js to toggle hidden class for the container card and success message
    [x] Updated attribution styling

--- script.js
+++ script.js
@@ -4,14 +4,20 @@ const form = document.querySelector('.cta__form');
 const emailEl = document.querySelector('.user-email');
 const dismissBtnEl = document.querySelector('.btn__dismiss');
 const emailInput = document.querySelector('input');
+const cardEl = document.querySelector('.section__cta');
+const successEl = document.querySelector('.form__success');
 
 function handleSubmit(e) {
   e.preventDefault();
   emailEl.textContent = emailInput.value;
+  cardEl.classList.toggle('hidden');
+  successEl.classList.toggle('hidden');
 }
 
 function dismiss() {
   emailInput.value = '';
+  cardEl.classList.toggle('hidden');
+  successEl.classList.toggle('hidden');
 }
 
 form.addEventListener('submit', handleSubmit);
